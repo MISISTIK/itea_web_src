8527906a10165f59a27a94e0b220b311103e1026
Tymoshok Dmytro <misistik@gmail.com>
2019-02-03 23:00:06 +0200

    Database work done

--- src/my/jdbc/DBWorker.java
+++ src/my/jdbc/DBWorker.java
@@ -4,11 +4,17 @@ import javax.xml.transform.Result;
 import java.sql.*;
 
 public class DBWorker {
-    Connection conn = null;
-    Statement st;
-    String url = "jdbc:mysql://s3.thehost.ua/itea2?" +
-            "user=helen&password=123456";
-    final static String GET_ANIMALS = "select * from ss";
+    private Connection conn = null;
+    private Statement st;
+//    String url = "jdbc:mysql://s3.thehost.ua/itea2?" +
+//            "user=helen&password=123456";
+    private String url = "jdbc:mysql://localhost/dbitea?" +
+                "user=mysql&password=mysql";
+    private final static String GET_USERS = "select * from users";
+    private final static String INSERT_USER = "insert into users " +
+            "(login, password, name, age, gender, address, comment) " +
+            "values " +
+            "(?,?,?,?,?,?,?)" ;
 
     public DBWorker() {
         try {
@@ -30,16 +36,27 @@ public class DBWorker {
         System.out.println("Connection obtained");
     }
 
-    public String getRows() {
+    public String getUsers() {
         StringBuilder buf = new StringBuilder();
-        buf.append("<table>");
+        buf.append("<table border='1'>");
         try {
-            ResultSet rs= st.executeQuery(GET_ANIMALS);
-
+            ResultSet rs = st.executeQuery(GET_USERS);
+            buf.append("<tr><td>Login</td><td>");
+            buf.append("Name").append("</td><td>");
+            buf.append("Age").append("</td><td>");
+            buf.append("Gender").append("</td><td>");
+            buf.append("Address").append("</td><td>");
+            buf.append("Comment");
+            buf.append("</td></tr>");
             while(rs.next()) {
-                buf.append("<tr><td>" + rs.getString(1)).append("</td><td>");
-                buf.append(rs.getString(2)).append("</td><td>");
-                buf.append(rs.getString(3)).append("</td></tr>");
+                buf.append("<tr><td>")
+                        .append(rs.getString(2)).append("</td><td>");
+                buf.append(rs.getString(4)).append("</td><td>");
+                buf.append(rs.getInt(5)).append("</td><td>");
+                buf.append(rs.getString(6)).append("</td><td>");
+                buf.append(rs.getString(7)).append("</td><td>");
+                buf.append(rs.getString(8));
+                buf.append("</td></tr>");
             }
             buf.append("</table>");
             System.out.println(buf.toString());
@@ -51,7 +68,46 @@ public class DBWorker {
         return buf.toString();
     }
 
-    public void close () {
+    public boolean checkUserByLogin(String login) {
+        final String sql = String.format("select * from users where login = '%s'",login);
+        try {
+            st.executeQuery(sql);
+            ResultSet rs = st.getResultSet();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public String registerUser(
+            String login,
+            String password,
+            String name,
+            String age,
+            String gender,
+            String address,
+            String comment
+    ) {
+        try {
+            PreparedStatement ps = conn.prepareStatement(INSERT_USER);
+            ps.setString(1,login);
+            ps.setString(2,password);
+            ps.setString(3,name);
+            ps.setInt(4,Integer.parseInt(age));
+            ps.setString(5,gender);
+            ps.setString(6,address);
+            ps.setString(7,comment);
+            ps.execute();
+
+            return "true";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "<font color = 'red'>SQLException : " + e.getMessage() + " SQLState : " + e.getSQLState() +"</font>";
+        }
+    }
+
+    public void close() {
         try {
             if (st != null) st.close();
             if (conn != null) conn.close();
--- src/my/jdbc/main.java
+++ src/my/jdbc/main.java
@@ -2,8 +2,19 @@ package my.jdbc;
 
 public class main {
     public static void main(String[] args) {
+
+//        String login = "any@mail.com";
+//        String password = "123";
+//        String name = "Lena";
+//        String age = "18";
+//        String gender = "M";
+//        String address = "Ukraine";
+//        String comment = "1";
+
         DBWorker db = new DBWorker();
-        db.getRows();
+//        db.getUsers();
+//        System.out.println(db.checkUserByLogin("admin@itea.ua"));
+//        db.registerUser(login,password,name,age,gender,address,comment);
         db.close();
     }
 }
